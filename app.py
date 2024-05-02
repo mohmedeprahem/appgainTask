218630eb008ee4658622dc3b5c652946ae087cf1
mohmedeprahem <m.eprahem.168@gmail.com>
2024-05-03 02:05:12 +0300

    feat: :sparkles: Connect to database

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+
+import os
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+mongo = PyMongo(app)
 
 @app.route("/")
 def hello_world():
