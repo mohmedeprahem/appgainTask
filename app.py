54075f4641574a6a17147057115257448237906e
mohmedeprahem <m.eprahem.168@gmail.com>
2024-05-03 22:58:17 +0300

    feat: :sparkles: Install bcrypt package

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, request
 from flask_pymongo import PyMongo
 from flask_swagger_ui import get_swaggerui_blueprint
-
+from flask_bcrypt import Bcrypt
 
 import os
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 mongo = PyMongo(app)
+
+bcrypt = Bcrypt(app)
 
 SWAGGER_URL = '/api/docs'
 API_URL = '/static/swagger.json'
@@ -20,5 +22,7 @@ swaggerui_blueprint = get_swaggerui_blueprint(
 app.register_blueprint(swaggerui_blueprint)
 
 from routes.item_routes import item_bp
+from routes.auth_routes import auth_bp
 
 app.register_blueprint(item_bp, url_prefix='/items')
+app.register_blueprint(auth_bp, url_prefix='/auth')
