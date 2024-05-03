757d67005c72c4c77f99e4f8f89b815959909a94
mohmedeprahem <m.eprahem.168@gmail.com>
2024-05-03 21:54:42 +0300

    feat: :sparkles: Validate incoming data in add item post API

--- routes/item_routes.py
+++ routes/item_routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request
 from app import mongo
+from schemas.item_schemas import ItemSchema
+from marshmallow import ValidationError
 
 item_bp = Blueprint('item', __name__)
 
@@ -7,6 +9,10 @@ db = mongo.db.items
 
 @item_bp.route('/', methods=['POST'])
 def add_items():
-    data = request.get_json()
-    db.insert_one(data)
-    return 'Item added successfully' , 201
+    try:
+        data = request.get_json()
+        ItemSchema().load(data)
+        db.insert_one(data)
+        return 'Item added successfully' , 201
+    except ValidationError as error:
+        return error.messages, 400
