5c1b0190cf71aaa2c245c2928cf9b35c9e6d38df
mohmedeprahem <m.eprahem.168@gmail.com>
2024-05-03 22:18:07 +0300

    feat: :sparkles: Add get items GET api

--- routes/item_routes.py
+++ routes/item_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from app import mongo
 from schemas.item_schemas import ItemSchema
 from marshmallow import ValidationError
@@ -16,3 +16,24 @@ def add_items():
         return 'Item added successfully' , 201
     except ValidationError as error:
         return error.messages, 400
+
+@item_bp.route('/', methods=['GET'])
+def get_items():
+    items = [item for item in db.find()]
+
+    # Convert ObjectId to string for JSON serialization
+    if items is not None:
+        for item in items:
+            item['_id'] = str(item['_id'])
+
+        return jsonify({
+            'success': True,
+            'statusCode': 200,
+            'items': items
+        }), 200
+    else:
+        return jsonify({
+            'success': False,
+            'statusCode': 404,
+            'message': 'No items found'
+        }), 404
