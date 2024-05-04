54f613d895af00178458445b972940bc331c8ad3
mohmedeprahem <m.eprahem.168@gmail.com>
2024-05-04 18:31:32 +0300

    feat: :sparkles: init flask_mail service

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask, request
 from flask_pymongo import PyMongo
 from flask_swagger_ui import get_swaggerui_blueprint
+from flask_mail import Mail, Message
 
 import os
 
 app = Flask(__name__)
+
+# init database
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 mongo = PyMongo(app)
 
+# init jwt
 app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY') or 'you-will-never-guess'
 
+# init swagger
 SWAGGER_URL = '/api/docs'
 API_URL = '/static/swagger.json'
 
@@ -17,6 +22,16 @@ swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
     config={'app_name': "Your API Documentation"})
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'jobjobjobpro@gmail.com'
+app.config['MAIL_PASSWORD'] = 'shfj etrz opcq pztc'
+app.config['MAIL_DEFAULT_SENDER'] = 'jobjobjobpro@gmail.com'
+app.config['MAIL_USE_SSL'] = False
+
+mail = Mail(app)
 
 app.register_blueprint(swaggerui_blueprint)
 
