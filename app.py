88e7d93094c4e774a14076b99f996e15c488dc1b
mohmedeprahem <m.eprahem.168@gmail.com>
2024-05-04 05:17:40 +0300

    feat: :sparkles: Import order_routes in app file

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 mongo = PyMongo(app)
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY') or 'you-will-never-guess'
 
 SWAGGER_URL = '/api/docs'
 API_URL = '/static/swagger.json'
@@ -22,6 +22,8 @@ app.register_blueprint(swaggerui_blueprint)
 
 from routes.item_routes import item_bp
 from routes.auth_routes import auth_bp
+from routes.order_routes import order_bp
 
 app.register_blueprint(item_bp, url_prefix='/items')
 app.register_blueprint(auth_bp, url_prefix='/auth')
+app.register_blueprint(order_bp, url_prefix='/orders')
